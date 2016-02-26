f91d5cd00d3184038d846620a25ddb42e100d4e7
vbabenkoff <vlad.babenko.1998@gmail.com>
2016-02-26 12:33:14 +0200

    Create main.c

---
+++ task1/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int max (float a, float b)
+{
+  if (a > b) {
+    return a;
+    
+  } else {
+    return b;
+}
+
+}
+
+int main ()
+{
+    int a = 2, b = 1, y;
+    y = max(a, b);
+
+   printf("%d", y);
+
+   return 0; 
+
+}
+
+Status API Training Shop Blog About Pricing
+© 2016 GitHub, Inc. Terms Privacy S
