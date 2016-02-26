6402c8875c2bd6c6128abc6724e01f26c1c5ac53
vbabenkoff <vlad.babenko.1998@gmail.com>
2016-02-26 12:29:47 +0200

    fixed code style

--- main.cpp
+++ main.cpp
@@ -3,32 +3,23 @@
 int max (float a, float b)
 
 {
-
-if (a > b) {
-
-return a;
-
-} else {
-
-return b;
-
+  if (a > b) {
+    return a;
+    
+  } else {
+    return b;
 }
 
 }
 
 int main ()
-
 {
+    int a = 2, b = 1, y;
+    y = max(a, b);
 
-int a = 2, b = 1, y;
+   printf("%d", y);
 
-y = max(a, b);
-
-printf("%d", y);
-
-
-
-return 0; 
+   return 0; 
 
 }
 
